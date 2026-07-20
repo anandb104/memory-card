70b9178a1c4b022a1998d72908315902db32d78a
anandb104 <210169445+anandb104@users.noreply.github.com>
2026-07-20 23:05:01 +0530

    Create carddiv for storing all cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
+import Carddiv from"./Carddiv.jsx"
 function App() {
   return (
     <>
     <Header/>
+    <Carddiv/>
     <Footer/>
     </>
   )
