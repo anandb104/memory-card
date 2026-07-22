417473e703036eb68364446a37a05a6554167657
anandb104 <210169445+anandb104@users.noreply.github.com>
2026-07-22 21:45:58 +0530

    Map the cards to each card

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card({difficulty,pokemon}){
+let ncards=0;
+if(difficulty=="easy")ncards=6;
+else if(difficulty=="medium")ncards=12;
+else ncards=20;
+return(
+    
+)
+}
--- src/components/carddiv.jsx
+++ src/components/carddiv.jsx
@@ -1,10 +1,14 @@
-export default function Carddiv({difficulty}){
+import Card from "./Card.jsx"
+export default function Carddiv({difficulty,cards}){
     let ncards=0;
     if(difficulty=="easy")ncards=6;
     else if(difficulty=="medium")ncards=12;
     else ncards=20;
     return(
-        <>
-        </>
+        cards.map(card=>{
+            <Card key={card.id}
+            pokemon={card}
+            difficulty={difficulty}/>
+        })
     )
 }
